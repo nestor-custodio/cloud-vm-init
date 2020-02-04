4630e9a6e4668765694d19d508aceb9c51511c91
Nestor Custodio <sakimorix@gmail.com>
2020-02-04 15:39:56 -0600

    remove: don't install Node/`nvm` or Ruby/`chruby`/`ruby-install` on init

--- init.sh
+++ init.sh
@@ -77,50 +77,10 @@ section "Setting user shell ..."
 
 
 
-section "Setting up nvm (nodejs) ..."
+section "Prepping for Ruby / Rails work ..."
 
-	git clone -q https://github.com/creationix/nvm.git ~/.nvm
-	cd ~/.nvm || exit
-	git checkout -q "$( git describe --abbrev=0 --tags )"
-	puts "source ~/.nvm/nvm.sh" >> ~/.zshrc
-
-	zsh -ic 'nvm install stable'
-	puts "stable" > ~/.nvmrc
-
-
-
-
-section "Setting up Ruby + Rails + Heroku ..."
-
-	# chruby v0.3.9 -- https://github.com/postmodern/chruby
-	puts "\nInstalling: chruby ...\n"
-		cd ~ || exit
-		wget -q -O chruby-installer.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
-		tar -xzvf chruby-installer.tar.gz > /dev/null && mv ./chruby-0.3.9 ./chruby-installer
-		cd ~/chruby-installer || exit
-		sudo make install > /dev/null
-		sudo ./scripts/setup.sh
-		cd ~ || exit
-		rm -rf ~/chruby-installer*
-		puts "source /usr/local/share/chruby/chruby.sh" >> ~/.zshrc
-		puts "source /usr/local/share/chruby/auto.sh"   >> ~/.zshrc
-
-
-	# ruby-install v0.7.0 -- https://github.com/postmodern/ruby-install
-	puts "\nInstalling: ruby-install ...\n"
-		cd ~ || exit
-		wget -q -O ri-installer.tar.gz https://github.com/postmodern/ruby-install/archive/v0.7.0.tar.gz
-		tar -xzvf ri-installer.tar.gz > /dev/null && mv ./ruby-install-0.7.0 ./ri-installer
-		cd ~/ri-installer || exit
-		sudo make install > /dev/null
-		cd ~ || exit
-		rm -rf ~/ri-installer*
-
-
-	puts "\nPrepping for Ruby / Rails work ...\n"
-		puts "ruby" > ~/.ruby-version
-		sudo apt-get -qqy install libpq-dev  # We can't build the 'pg' gem without this.
-		/snap/bin/heroku plugins:install heroku-accounts  # Simple juggling of Heroku credentials.
+	sudo apt-get -qqy install libpq-dev  # We can't build the 'pg' gem without this.
+	/snap/bin/heroku plugins:install heroku-accounts  # Simple juggling of Heroku credentials.
 
 
 
